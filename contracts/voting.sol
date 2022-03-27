f94a5e00380f13e6ee79ebd4d6c9a83aff5e71e0
kavania2002 <kavan155gondalia@gmail.com>
2022-03-27 11:04:49 +0530

    fix/getAll() for proposals

--- contracts/voting.sol
+++ contracts/voting.sol
@@ -1,82 +1,99 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-pragma experimental ABIEncoderV2;
+pragma solidity >=0.7.0 <0.9.0;
 
 contract Voting {
+
     // struct for each proporsal
     // struct defines datatype of each proposals
-    struct Info {
+    struct Info{
         string name;
         string description;
         address tokenAd;
+        uint leftCount;
+        uint rightCount;
+        address admin;
+        // map to get whether a user selected yes or no
+        mapping(address => bool) userValue; 
     }
-
     // array of struct
     // array of each propasals
     Info[] Informations;
+    string[] private allName;
+    address[] private allAds;
 
-    // map to get whether a user selected yes or no
-    mapping(address => bool) userValue;
 
+    
     address[] private active;
     address[] private completed;
-
     function startProposal(address ad) public {
         active.push(ad);
     }
-
     function endProposal(address ad) public {
-        for (uint256 i = 0; i < active.length; i++) {
-            if (ad == active[i]) {
-                active[i] = active[active.length - 1];
+        for(uint i=0; i < active.length; i++){
+            if( ad == active[i]){
+                active[i] = active[active.length-1];
                 active.pop();
                 completed.push(ad);
             }
         }
     }
-
     function displayActiveList() public view returns (address[] memory) {
-        return active;
+    return active;
     }
-
     function displayCompletedList() public view returns (address[] memory) {
-        return completed;
+    return completed;
     }
 
     // fetch all the proposals for the dashboard
-    function getAll() public view returns (Info[] memory) {
-        return Informations;
+    function getAll() public returns (string[] memory names, address[] memory tkAds) {
+        for (uint i = 0; i<Informations.length; i++) {
+            allName.push(Informations[i].name);
+            allAds.push(Informations[i].tokenAd);
+        }
+
+        return (allName, allAds);
     }
 
     // fetch only one propasals for sepecific page's propasals
-    function getOne(address tkAd) public view returns (Info memory) {
-        uint256 n = Informations.length;
-        for (uint256 i = 0; i < n; i++) {
+    function getOne(address tkAd) public view returns (string memory name, address tkA) {
+        uint n = Informations.length;
+        for (uint i = 0; i<n; i++) {
             if (Informations[i].tokenAd == tkAd) {
-                return Informations[i];
+                return (Informations[i].name, Informations[i].tokenAd);
             }
         }
-        Info memory temp;
-        return temp;
-    }
+    } 
 
     // get user selected value via address
-    function getUserValue(address tkAd) public view returns (bool value) {
-        return userValue[tkAd];
+    function getUserValue(address userAd, address proposalsAd) public view returns (bool value) {
+        for (uint i = 0; i<Informations.length; i++) {
+            if (Informations[i].tokenAd == proposalsAd) {
+                return Informations[i].userValue[userAd];
+            }
+        }
     }
 
     // set each propasals
-    function setInfo(
-        string memory name,
-        string memory description,
-        address tkAd
-    ) public {
-        Info memory temp = Info(name, description, tkAd);
-        Informations.push(temp);
+    function setInfo(string memory name, string memory description, address tkAd) public {
+        Info storage info = Informations.push();
+        info.name=name;
+        info.description = description;
+        info.tokenAd = tkAd;
     }
 
     // set each user's value
-    function setUser(address userAd, bool value) public {
-        userValue[userAd] = value;
+    function setUser(address pAd, address userAd, bool value) public {
+        uint n = Informations.length;
+        for (uint i = 0; i<n; i++) {
+            if (Informations[i].tokenAd == pAd) {
+                Informations[i].userValue[userAd] = value;
+                if (value == true) {
+                    Informations[i].leftCount++;
+                } else {
+                    Informations[i].rightCount++;
+                }
+            }
+        }
     }
+
 }
